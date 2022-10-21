8f651980c2ec57bbd80af46201fa077acde9c175
MujahidRopiq <92687253+MujahidRopiq@users.noreply.github.com>
2022-10-21 17:25:26 +0700

    Update login.php

--- login.php
+++ login.php
@@ -22,31 +22,5 @@ $data=mysqli_fetch_array($query);
                 <img src="img/<?PHP echo $data['photo']; ?>" width="50%" height="10%">
               </div>
 
-				 <div class="control-group normal_text">  <h2>Login Administrator</h2><h3><?php echo $data['nama_sekolah']; ?></h3>
-                 </div>
-                <div class="control-group">
-                    <div class="controls">
-                        <div class="main_input_box">
-                            <span class="add-on bg_lg"><i class="icon-user"> </i></span><input name='username' type="text" placeholder="Username" />
-                        </div>
-                    </div>
-                </div>
-                <div class="control-group">
-                    <div class="controls">
-                        <div class="main_input_box">
-                            <span class="add-on bg_ly"><i class="icon-lock"></i></span><input name='password'type="password" placeholder="Password" />
-                        </div>
-                    </div>
-                </div>
-                <div class="form-actions">
-                    <span class="pull-left"><input type="reset" name="reset" value="reset"class="flip-link btn btn-danger"></span>
-                    <span class="pull-right"><input type="submit" name="login" value="Login"class="btn btn-success"></span>
-                </div>
-            </form>
-        </div>
-
-        <script src="js/jquery.min.js"></script>
-        <script src="js/matrix.login.js"></script>
-    </body>
 
 </html>
